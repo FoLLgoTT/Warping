76be0f34fd8f39d85cf8e4791851ca489905e32c
FoLLgoTT <95616074+FoLLgoTT@users.noreply.github.com>
2022-05-23 11:20:27 +0200

    Update warping.hlsl

--- warping.hlsl
+++ warping.hlsl
@@ -1,15 +1,16 @@
+static const float widthNative = 3840; 				// set native x resolution here
+
 static const float distortionFactorX = 0.0; 		// higher = more curved distortion
 static const float distortionFactorY = 0.0; 		// higher = more curved distortion
 static const float distortionCenterX = 1.0; 		// 1 = symmetrical to y. 0 = only bottom. 2 = only top.
 static const float distortionCenterY = 1.0; 		// 1 = symmetrical to y. 0 = only bottom. 2 = only top.
-static const float distortionBowY = 1.0;		// 1 = none. >1 bow to bottom. <1 bow to top
+static const float distortionBowY = 1.0;			// 1 = none. >1 bow to bottom. <1 bow to top
 
-static const float trapezTop = 1.0;			// trapezoid distortion factor for the top of the picture
+static const float trapezTop = 1.0;				// trapezoid distortion factor for the top of the picture
 static const float trapezBottom = 1.0;			// trapezoid distortion factor for the bottom of the picture
 
-static const float linearityCorrectionX = 1.0;		// corrects horizontal linearity for anamorphic lens
-static const float linearityCorrectionY = 1.0;		// corrects vertical linearity for anamorphic lens
-
+static const float linearityCorrectionX = 1.0;	// corrects horizontal linearity for anamorphic lens
+static const float linearityCorrectionY = 1.0;	// corrects vertical linearity for anamorphic lens
 
 
 SamplerState samp : register(s0);
@@ -17,7 +18,6 @@ SamplerState samp : register(s0);
 float4 p0 :  register(c0);
 #define width  (p0[0])
 #define height (p0[1])
-
 
 float BicubicWeight(float d)
 {
@@ -56,6 +56,7 @@ float3 Bicubic(float2 uv, float2 InvResolution)
 			
 			if(coord.x >= 0.0 && coord.x <= 1.0 && coord.y >= 0.0 && coord.y <= 1.0) // ignore pixels outside the picture
 				col += w * tex2D(samp, coord).rgb;
+				
 			weight += w;
 		}
 	}
@@ -66,25 +67,32 @@ float3 Bicubic(float2 uv, float2 InvResolution)
 
 float4 main(float2 uv : TEXCOORD0) : COLOR
 {
+	float zoom = width / widthNative;
+	float xZoomed = (uv.x - 0.5) * zoom + 0.5;
+	float yZoomed = (uv.y - 0.5) * zoom + 0.5;
+		
 	// perform distortion for curved screen (follows a parabola)
-	uv.x += distortionFactorX * (-2.0 * uv.x + distortionCenterX) * uv.y * (uv.y - 1.0);	
-	uv.y += distortionFactorY * (-2.0 * pow(uv.y, distortionBowY) + distortionCenterY) * uv.x * (uv.x - 1.0);
+	uv.x += distortionFactorX * (-2.0 * xZoomed + distortionCenterX) * yZoomed * (yZoomed - 1.0);	
+	uv.y += distortionFactorY * (-2.0 * pow(yZoomed, distortionBowY) + distortionCenterY) * xZoomed * (xZoomed - 1.0);
 	
 	// trapezoid
-	float size = lerp(trapezTop, trapezBottom, uv.y);
-    	float reciprocal = 1.0 / size;
-    	uv.x = uv.x * reciprocal + (1.0 - reciprocal) / 2.0;
+	if(trapezTop != 1.0 || trapezBottom != 1.0)
+	{
+		float size = lerp(trapezTop, trapezBottom, yZoomed);
+		float reciprocal = 1.0 / size;
+		uv.x = uv.x * reciprocal + (1.0 - reciprocal) / 2.0;
+	}
 	
 	// linearity
 	if(linearityCorrectionX != 1.0)
 	{
-		float x = uv.x - 0.5;
-		uv.x = lerp(x * abs(x) * 2.000001, x, linearityCorrectionX) + 0.5;
+		float x = xZoomed - 0.5;
+		uv.x = lerp(x * abs(x) * 2.000001, uv.x - 0.5, linearityCorrectionX) + 0.5;
 	}
 	if(linearityCorrectionY != 1.0)
 	{
-		float y = uv.y - 0.5;
-		uv.y = lerp(y * abs(y) * 2.00001, y, linearityCorrectionY) + 0.5;
+		float y = yZoomed - 0.5;
+		uv.y = lerp(y * abs(y) * 2.00001, uv.y - 0.5, linearityCorrectionY) + 0.5;
 	}
 
 	float3 result = Bicubic(uv, 1.0 / float2(width, height));
